0aed77196efd8fae1ab989019b71b188ac38705d
Piyushkanojia42 <117583829+Piyushkanojia42@users.noreply.github.com>
2026-01-14 07:33:56 +0000

    some changes

--- 01_basics/03_strings.js
+++ 01_basics/03_strings.js
@@ -7,6 +7,23 @@ const gameName = new String("Piyush Kanojia");
 //console.log(gameName[1]);
 //console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
 
+//console.log(gameName.charAt(3));
+//console.log(gameName.indexOf('i'));
+
+//const newString = gameName.substring(0,6);
+//console.log(newString);
+
+const anotherString = gameName.slice(-8, -1);
+//console.log(anotherString);
+
+const newstring = "   Piyush    ";
+//console.log(newstring);
+//console.log(newstring.trim());
+
+const url = "https://piyushkanojia.com/myprofile/Piyush%20kanojia";
+//console.log(url.replace("%20","-"));
+
+console.log(url.includes("PUBG"));
