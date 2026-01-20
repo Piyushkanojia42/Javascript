eaf8116f946ba6a0d212b4124b77f80442720db7
Piyushkanojia42 <117583829+Piyushkanojia42@users.noreply.github.com>
2026-01-20 13:30:33 +0000

    new folder

---
+++ 02_basics/02_array.js
@@ -0,0 +1,8 @@
+const marvel_heroes = ["Iron Man", "Captain America", "Thor", "Hulk"];
+const dc_heroes = ["Batman", "Superman", "flash"];
+
+const all_heroes = marvel_heroes.concat(dc_heroes);
+
+console.log(all_heroes);
+// Output
+
